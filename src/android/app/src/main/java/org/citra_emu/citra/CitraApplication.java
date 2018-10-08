279bf75e0dc008a3f2b1774ee6714ba5b87be9ba
BreadFish64 <mohror64@gmail.com>
2018-10-08 17:01:05 -0500

    android: create bare application

---
+++ src/android/app/src/main/java/org/citra_emu/citra/CitraApplication.java
@@ -0,0 +1,5 @@
+package org.citra_emu.citra;
+
+import android.app.Application;
+
+public class CitraApplication extends Application {}
---
+++ src/android/app/src/main/java/org/citra_emu/citra/ui/main/MainActivity.java
@@ -0,0 +1,14 @@
+package org.citra_emu.citra.ui.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.citra_emu.citra.R;
+
+public final class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
